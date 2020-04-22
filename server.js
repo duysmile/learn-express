9400865b817a98d7aab66c0c6ddc214c4aa37781
Glitch (nova-stitch-beech) <none>
2020-04-22 15:03:03 +0000

    🐾☂️ Updated with Glitch

--- server.js
+++ server.js
@@ -5,15 +5,18 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require('express');
 const app = express();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({ todos: [] })
+  .write();
+
+const Todo = db.get('todos');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const todos = [
-  {id: 1, content: 'Ăn'},
-  {id: 2, content: 'Ngủ'},
-  {id: 3, content: 'Code'},
-];
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -25,11 +28,12 @@ app.get('/', (request, response) => {
 
 app.get('/todos', (req, res) => {
   const q = req.query.q;
+  
   if (q) {
-    const matchTodos = todos.filter(todo => {
+    const matchTodos = Todo.filter(todo => {
       return todo.content.toLowerCase()
         .includes(q.toLowerCase());
-    });
+    }).value();
   
     return res.render('todo/list', {
       todos: matchTodos,
@@ -37,6 +41,7 @@ app.get('/todos', (req, res) => {
     });  
   }
   
+  const todos = Todo.value();
   return res.render('todo/list', {
     todos,
   });
@@ -48,11 +53,12 @@ app.get('/todos/create', (req, res) => {
 
 app.post('/todos/create', (req, res) => {
   const content = req.body.content;
+  const todos = db.get('todos');
   const id = todos.length;
-  todos.push({
+  db.get('todos').push({
     id: id,
     content: content,
-  });
+  }).write();
   return res.redirect('/todos');
 });
 

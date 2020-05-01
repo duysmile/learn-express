cfbf4b71505bb2ea45ddccad3fef9ac05ef6126c
Duy Nguyen <duy.nguyen@gonjoy.com>
2020-05-02 00:09:37 +0700

    fix mongo

--- server.js
+++ server.js
@@ -24,40 +24,9 @@ const cartApiRoute = require('./api/routes/cart.route');
 const authMiddleware = require('./middlewares/auth.middleware');
 const sessionMiddleware = require('./middlewares/session.middleware');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser(process.env.SESSION_SECRET));
-app.use(csrf({
-  cookie: true,
-}));
-app.use(sessionMiddleware);
-
-app.use(express.static('public'));
-
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.redirect('/auth/login');
-});
-
-app.use('/users', csrf({ cookie: true }), authMiddleware.requireAuth, userRoute);
-app.use('/books', csrf({ cookie: true }), bookRoute);
-app.use('/transactions', csrf({ cookie: true }), authMiddleware.requireAuth, transactionRoute);
-app.use('/auth', authRoute);
-app.use('/cart', cartRoute);
-
-app.use('/api', authApiRoute);
-app.use('/api/transactions', transactionApiRoute);
-app.use('/api/books', bookApiRoute);
-app.use('/api/carts', cartApiRoute);
-
-app.use(function (err, req, res, next) {
-  console.error(err);
-  res.status(500).render('common/error');
-});
+app.get('/', (req, res) => {
+  res.send('ok');
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, () => {
